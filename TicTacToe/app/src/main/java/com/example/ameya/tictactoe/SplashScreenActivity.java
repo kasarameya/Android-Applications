7a7a447d2940b27da219d0978d311ec047fa1d72
kasarameya <kasarameya@gmail.com>
2018-03-20 16:40:11 -0500

    TicTacToe
    This is a game for android applications where if the user if able to get 3 x's in a line then the user is declared as the winner of the game.

---
+++ TicTacToe/app/src/main/java/com/example/ameya/tictactoe/SplashScreenActivity.java
@@ -0,0 +1,36 @@
+package com.example.ameya.tictactoe;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash_screen with a timer. This will be useful when you
+             * want to show case your app logoo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity\
+
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
+
